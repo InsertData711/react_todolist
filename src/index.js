36606af42b033a6705d90252d5f3752617430fca
InsertData711 <donghwi0711@iclould.com>
2024-05-09 14:03:18 +0900

    fixed file for index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <Provider store={store}>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+reportWebVitals();
